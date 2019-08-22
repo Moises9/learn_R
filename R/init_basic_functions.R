309db23be7ae027f2282aa0baeba2fd449e57f20
Moises9 <moises.estat@gmail.com>
2019-08-21 23:39:22 -0300

    Definição escopo da apresentação

--- R/init_basic_functions.R
+++ R/init_basic_functions.R
@@ -1,8 +1,15 @@
 
 # BIBLIOTECAS UTILIZADAS=======================================================
 install.packages("dplyr")
-library(dplyr)
 
+library(dplyr)
+library(esquisse)
+library(breakDown)
 
 #===============================================================================#
+
+df.vagas_db <- readRDS("data_vagas_db/data_output/vagas_id.rda")
+
+data <- esquisser(df.vagas_db)
+
 
