25c49f9a596a79876f29d182e493c78e40bbeb4d
Moises9 <moises.estat@gmail.com>
2019-08-21 23:40:49 -0300

    Descrição do tema de apresentação

---
+++ R/decritiva_meetup.R
@@ -0,0 +1,15 @@
+
+
+#===============================================#
+
+
+Disponibilidades e posibilidades de analises de volumosas 
+base dados  possibilita  insihts cada vez mais profundos, com 
+surgimentos de soluções a problemas de ambientes e individuos.
+  
+Com isso, buscaremos nos interar nas seguintes questões:
+
+O que é dados secundários?
+Qual a importancia dos dados secundários?
+Como informações secundárias pode ajudar em tomadas de decisões?
+
